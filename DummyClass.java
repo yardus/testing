720d7ed373983deca85ca61323b5ef98e64a2158
J.Bachorik <jaroslav@unraveldata.com>
2016-05-20 18:15:18 +0200

    Update DummyClass.java

--- DummyClass.java
+++ DummyClass.java
@@ -1,3 +1,9 @@
 public class DummyClass {
   public static final String DUMMY = "dummy";
+     
+     private void
+        helloWorld() 
+     {
+       System.out.println("hello world");        
+     }
 }
